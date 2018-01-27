bfffe0d35d8c135fdd2052d412e5e519355e1f69
Jeff H <jeffrey92@gmail.com>
2018-01-26 18:29:29 -0800

    bump

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ PACKAGES = [ 'psp' ]
 META_PATH = os.path.join('psp', '__init__.py')
 KEYWORDS = [ 'color' 'colour' 'terminal' 'text' 'ansi' 'minimalist' ]
 CLASSIFIERS = [
+    'Development Status :: 2 - Pre-Alpha',
     'Environment :: Console',
     'Intended Audience :: Developers',
     'Operating System :: OS Independent',
@@ -20,7 +21,7 @@ CLASSIFIERS = [
 if __name__ == '__main__':
     setup(
         name=NAME,
-        version='0.0.2',
+        version='0.0.3',
         description='python color printer - minimalist color printing',
         long_description='python simple (color) printing - simple barebones wrappers for 256-color stdout.',
         license='MIT',
