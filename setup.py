cccf381d2357576b56a6814e70530b0f63dde0d4
Jeff H <jeffrey92@gmail.com>
2018-01-26 18:55:25 -0800

    0.0.4

--- setup.py
+++ setup.py
@@ -11,6 +11,10 @@ CLASSIFIERS = [
     'Environment :: Console',
     'Intended Audience :: Developers',
     'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3.1',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
@@ -21,7 +25,7 @@ CLASSIFIERS = [
 if __name__ == '__main__':
     setup(
         name=NAME,
-        version='0.0.3',
+        version='0.0.4',
         description='python color printer - minimalist color printing',
         long_description='python simple (color) printing - simple barebones wrappers for 256-color stdout.',
         license='MIT',
@@ -30,8 +34,8 @@ if __name__ == '__main__':
         author_email='jeffrey92@gmail.com',
         maintainer='Jeff Hufford',
         keywords=KEYWORDS,
-        packages=find_packages('psp'),
-        package_dir={'psp': 'psp'},
+        packages=['psp'],
+        #package_dir={'psp': ''},
         classifiers=CLASSIFIERS,
-        python_requires='>=3.5',
+        python_requires='>=3.1',
     )
